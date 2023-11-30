b29091154814193f636ca1b8cb30cb233d401ac0
Xoexoe <xoe@xoe>
2023-11-30 00:32:08 -0800

    unknown change why not

--- keyboards/splitkb/aurora/helix/keymaps/steno_xoe/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/steno_xoe/keymap.c
@@ -18,11 +18,11 @@ enum my_helix_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
-		KC_GRV,			KC_1,			KC_2,			KC_3,			KC_4,			KC_5,													KC_6,			KC_7,			KC_8,			KC_9,			KC_0,			KC_DEL,
-       KC_TAB,	KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,													KC_Y,			KC_U,			KC_I,			KC_O,			KC_P,			KC_BSPC,
-       KC_CAPS,	KC_A,			KC_S,			KC_D,			KC_F,			KC_G,													KC_H,			KC_J,			KC_K,			KC_L,			KC_SCLN,	KC_QUOT,
-       KC_LSFT,	KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,		G(C(KC_X)),	SILENT,		KC_N,			KC_M,			KC_COMM,	KC_DOT,		KC_SLSH,	KC_ENT ,
-       XXXXXXX,	XXXXXXX,	KC_LCTL,	KC_LALT,	KC_LGUI, 	KC_SPC,	NUMBER,			SYMBOL,		KC_SPC,		KC_RSFT,	KC_LEFT,	KC_UP,		KC_DOWN,	KC_RGHT
+			KC_GRV,		KC_1,			KC_2,			KC_3,			KC_4,			KC_5,													KC_6,			KC_7,			KC_8,			KC_9,			KC_0,			KC_DEL,
+      KC_TAB,		KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,													KC_Y,			KC_U,			KC_I,			KC_O,			KC_P,			KC_BSPC,
+      KC_CAPS,	KC_A,			KC_S,			KC_D,			KC_F,			KC_G,													KC_H,			KC_J,			KC_K,			KC_L,			KC_SCLN,	KC_QUOT,
+      KC_LSFT,	KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,		G(C(KC_X)),	SILENT,		KC_N,			KC_M,			KC_COMM,	KC_DOT,		KC_SLSH,	KC_ENT ,
+      XXXXXXX,	XXXXXXX,	KC_LCTL,	KC_LALT,	KC_LGUI, 	KC_SPC,	NUMBER,			SYMBOL,		KC_SPC,		KC_RSFT,	KC_LEFT,	KC_UP,		KC_DOWN,	KC_RGHT
 		),
 
 	[_STENO] = LAYOUT(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		XXXXXXX,	STN_N1,		STN_N2,		STN_N3,		STN_N4,		XXXXXXX,												XXXXXXX,	STN_N5,		STN_N6,		STN_N7,		STN_N8,		STN_N9,
 		XXXXXXX,	STN_S1,		STN_TL,		STN_PL,		STN_HL,		STN_ST1,												STN_ST3,	STN_FR,		STN_PR,		STN_LR,		STN_TR,		STN_DR,
 		KC_LSFT,	STN_S2,		STN_KL,		STN_WL,		STN_RL,		STN_ST2,	G(C(KC_X)),	SILENT,		STN_ST4,	STN_RR,		STN_BR,		STN_GR,		STN_SR,		STN_ZR,
-		KC_ESC,		XXXXXXX,	XXXXXXX,	KC_LALT,	STN_A,		STN_O,		NUMBER,			SYMBOL,		STN_E,		STN_U,		XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX
+		XXXXXXX,	XXXXXXX,	XXXXXXX,	KC_LALT,	STN_A,		STN_O,		NUMBER,			SYMBOL,		STN_E,		STN_U,		XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX
 		),
 
 	[_SILENT] = LAYOUT(
